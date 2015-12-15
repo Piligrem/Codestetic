9708e84dd8d257128ac1be283f38a349aece727f
Piligrem <KucherenkoIT@mail.ru>
2015-12-15 22:58:31 +0600

    https://github.com/Piligrem/Codestetic.git

---
+++ Codestetic.Core/Domain/Documents/Document.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Codestetic.Core.Domain.Directory;
+using Codestetic.Core.Domain.Employees;
+
+namespace Codestetic.Core.Domain.Documents
+{
+    public class Document : BaseEntity
+    {
+        #region Fields
+        private ICollection<DocumentDetails> _documentDetails;
+        #endregion Fields
+
+        #region Constructors
+        public Document()
+        {
+            Date = DateTime.Now;
+            Status = DocumentStatus.Pending;
+        }
+        #endregion Constructors
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the (formatted) document number
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// Gets or sets the (formatted) document date and time
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Gets or sets the document type id 
+        /// </summary>
+        public int DocumentTypeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the document type id 
+        /// </summary>
+        public virtual Guid TypeId { get; set; } // ???
+
+        /// <summary>
+        /// Gets or sets a value Region identifier (City, Provance)
+        /// </summary>
+        public int RegionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value Author identifier
+        /// </summary>
+        public int AuthorId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value Comment text
+        /// </summary>
+        public string Comment { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value last update document in format UTC
+        /// </summary>
+        public DateTime? UpdatedOnUtc { get; set; }
+
+        /// <summary>
+        ///  Gets or sets a value indicating whether the entity has been deleted (status document - Active, Deleted)
+        /// </summary>
+        public int StatusId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value status document (Active, Deleted)
+        /// </summary>
+        public object Pattern { get; set; } // ???
+        #endregion Properties
+
+        #region Navigation properties
+        public virtual string DocumentType { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value base document
+        /// </summary>
+        public virtual object BaseDocument { get; set; } // ???
+
+        /// <summary>
+        /// Gets or sets value DocumentDetails
+        /// </summary>
+        public virtual ICollection<DocumentDetails> DocumentDetails
+        {
+            get { return _documentDetails ?? (_documentDetails = new List<DocumentDetails>()); }
+            protected set { _documentDetails = value; }
+        }
+        public virtual Region Region { get; set; }
+        public virtual Employee Author { get; set; }
+        #endregion Navigation properties
+
+        #region Custom properties
+        /// <summary>
+        /// Gets or sets the order status
+        /// </summary>
+        public virtual DocumentStatus Status
+        {
+            get { return (DocumentStatus)this.StatusId; }
+            set { this.StatusId = (int)value; }
+        }
+        #endregion Custom properties
+    }
+}
---
+++ Codestetic.Core/Domain/Documents/DocumentDetails.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Codestetic.Core.Domain.Documents
+{
+    /// <summary>
+    /// Represents an document details
+    /// </summary>
+    public class DocumentDetails : BaseEntity
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the document identifier
+        /// </summary>
+        public int DocumentId { get; set; }
+
+        /// <summary>
+        /// Gets or sets document details (string for XML Serialization)
+        /// </summary>
+        public string Details { get; set; }
+        #endregion Properties
+
+        #region Navigation properties
+        /// <summary>
+        /// Gets or sets the document
+        /// </summary>
+        public virtual Document Document { get; set; }
+        #endregion Navigation properties
+
+        #region Custom properties
+        /// <summary>
+        /// Gets or sets document details fields
+        /// </summary>
+        public virtual IList<Field> Fields { get; set; } // ???
+        #endregion Custom properties
+    }
+}
