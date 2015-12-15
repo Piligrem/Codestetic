c5cd1d3850585748ba4c6cf61c87bd507ed206fb
Piligrem <KucherenkoIT@mail.ru>
2015-12-16 01:12:10 +0600

    https://github.com/Piligrem/Codestetic.git

---
+++ Codestetic.Core/Utilities/ActionDisposable.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace Codestetic.Utilities
+{
+
+    /// <summary>
+    /// Allows action to be executed, when it is disposed
+    /// </summary>
+    internal struct ActionDisposable : IDisposable
+    {
+        readonly Action _action;
+
+        public static readonly ActionDisposable Empty = new ActionDisposable(() => { });
+
+        public ActionDisposable(Action action)
+        {
+            _action = action;
+        }
+
+        public void Dispose()
+        {
+            _action();
+        }
+
+    }
+
+
+}
