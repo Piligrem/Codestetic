90953d8b220d1e6ded557a72b3c8c72c66569e6e
Piligrem <KucherenkoIT@mail.ru>
2015-12-16 22:35:01 +0600

    https://github.com/Piligrem/Codestetic.git

---
+++ Codestetic.Core/Events/AppInitScheduledTasksEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using Codestetic.Core.Domain.Tasks;
+
+namespace Codestetic.Core.Events
+{
+	/// <summary>
+	/// to initialize scheduled tasks in Application_Start
+	/// </summary>
+	/// <remarks>codehint: sm-add</remarks>
+	public class AppInitScheduledTasksEvent
+	{
+		public List<ScheduleTask> ScheduledTasks { get; set; }
+	}
+}
